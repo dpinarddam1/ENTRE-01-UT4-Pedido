fd99ae69bd21c9cc85022861c43aa87dbfd32223
dpinarddam1 <55190497+dpinarddam1@users.noreply.github.com>
2019-10-30 19:40:14 +0100

    Entrega1-UT4-Pedido-LineaPedido
    Lo he tenido que subir aquí ya que no me está dejando hacer push

---
+++ Pedido-LineaPedido/Pedido.java
@@ -0,0 +1,85 @@
+
+
+/**
+ * Modela un pedido realizado por un cliente en una determinada fecha
+ * El pedido incluye dos líneas de pedido que describen a cada uno de los dos
+ * productos comprados en el pedido
+ */
+public class Pedido
+{
+    private final double IVA = 0.21;  // iva a aplicar
+    private Fecha fecha;
+    private Cliente cliente;
+    private LineaPedido linea1;
+    private LineaPedido linea2;
+
+    /**
+     * Constructor  
+     */
+    public Pedido()    {
+        fecha = this.fecha;
+        cliente = this.cliente;
+        
+    }
+
+    // /**
+     // * accesor para la fecha del pedido
+     // */
+    // public   getFecha() {
+         // return fecha;
+    // }
+
+    // /**
+     // * accesor para el cliente
+     // */
+    // public   getCliente() {
+         
+    // }
+    
+    
+    // /**
+     // * calcular y devolver el importe total del pedido sin Iva
+     // */
+    // public   getImporteAntesIva() {
+         
+    // }
+
+    // /**
+     // * calcular y devolver el iva a aplicar
+     // */
+    // public   getIva() {
+         
+    // }
+
+    // /**
+     // * calcular y devolver el importe total del pedido con Iva
+     // */
+    // public   getImporteTotal() {
+         
+    // }
+
+    // /**
+     // * Representación textual del pedido
+     // * (ver enunciado)
+     // */
+    // public String toString() {
+        
+    // }
+    
+    
+    // /**
+     // * devuelve true si el pedido actual es más antiguo que el recibido 
+     // * como parámetro
+     // */
+    // public boolean masAntiguoQue(Pedido otro) {
+         
+    // }
+    
+     // /**
+     // * devuelve una referencia al pedido actual
+     // */
+    // public    getPedidoActual() {
+        
+    // }
+
+}
